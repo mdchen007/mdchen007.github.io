4e978e32503000428c5a120de1d26992995cbe85
タピオカ狐 <magneticchen@gmail.com>
2025-09-15 05:34:16 -0400

    Built and published by script.

---
+++ build.sh
@@ -0,0 +1,4 @@
+npm run build
+touch ./docs/.nojekyll
+# touch ./docs/CNAME
+# echo "tapiocafox.com" > ./docs/CNAME
---
+++ build_and_publish.sh
@@ -0,0 +1,4 @@
+. ./build.sh
+git add --all
+git commit -m "Built and published by script."
+git push origin main
